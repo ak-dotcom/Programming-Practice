3e61929d07dc70af3b2a6a7843e092836670412f
ak-dotcom <136290586+ak-dotcom@users.noreply.github.com>
2026-05-07 05:21:03 +0530

    Implement pattern printing in pattern1.c

--- C/Pattern/pattern1.c
+++ C/Pattern/pattern1.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+int main()
+   {  
+     for(int i = 1; i<5; i++) // Row
+     {
+       for(int j =1;j<6; j++) // Column
+       {
+         printf("*");
+           
+       }
+       printf("\n");
+     
+   }
+      return 0;
+    }
+   
