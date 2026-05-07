71eba8ae08711898454da3a0d6b9864cc36c3a4e
ak-dotcom <136290586+ak-dotcom@users.noreply.github.com>
2026-05-08 04:33:55 +0530

    Add pyramid pattern printing program
    Implement a program to print a pyramid pattern based on user input for the number of rows.

--- C/Pattern/pattern3.c
+++ C/Pattern/pattern3.c
@@ -1,1 +1,34 @@
 
+#include <stdio.h>
+int main()
+{
+    int i, j, rows, k = 0;
+    printf("Enter the number of rows: \n");
+    
+    // taking input for number of rows
+    scanf("%d", &rows);
+    
+    // Outer loop to handle number of rows
+    for (i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= rows - i; j++)
+        {
+            // Printing spaces
+            printf("  ");
+        }
+        for(k=0;k<= (2*i - 1);k++)
+        {
+            printf("*");
+        }
+        // Ending line after each row
+        printf("\n");
+    }
+
+  /*      *
+        ***
+      *****
+    *******
+  *********
+***********     */
+    return 0;
+}
